e34aa4031c2c4848d5d86fe15938dd0dea6df8f9
Yuchen-TNG <tngyc-wm24@student.tarc.edu.my>
2025-11-21 15:40:39 +0800

    Add project files.

---
+++ Mini Project Assignment_Y2S2/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Mini_Project_Assignment_Y2S2.Models;
+
+namespace Mini_Project_Assignment_Y2S2.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Mini Project Assignment_Y2S2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Mini_Project_Assignment_Y2S2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
