b6167d4feaeccdca735bf1768294270b4512c47d
TanKZ67 <tankz-wm24@student.tarc.edu.my>
2025-11-27 17:10:37 +0800

    hi

---
+++ Mini Project Assignment_Y2S2/wwwroot/js/index.js
@@ -0,0 +1,7 @@
+﻿document.getElementById("postButton").addEventListener("click", () => {
+
+    const bt1 = document.getElementById("foundButton");
+    const bt2 = document.getElementById("postButton");
+
+    [bt1.innerText, bt2.innerText] = [bt2.innerText, bt1.innerText];
+});
