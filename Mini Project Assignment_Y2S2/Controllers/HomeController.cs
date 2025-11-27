95bd524a5c766f793e0ea752091fe77c1e41b9c3
Yuchen-TNG <tngyc-wm24@student.tarc.edu.my>
2025-11-27 12:40:12 +0800

    ok

--- Mini Project Assignment_Y2S2/Controllers/HomeController.cs
+++ Mini Project Assignment_Y2S2/Controllers/HomeController.cs
@@ -28,5 +28,15 @@ namespace Mini_Project_Assignment_Y2S2.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public IActionResult CreatePost()
+        {
+            return View("CreatePost");
+        }
+
+        public IActionResult VerifyPost()
+        {
+            return View();
+        }
     }
 }
