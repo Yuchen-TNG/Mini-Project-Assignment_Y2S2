45c0ae79af9d146d5beb8c82de34600935ceafdc
Yuchen-TNG <tngyc-wm24@student.tarc.edu.my>
2025-12-04 23:33:29 +0800

    ok

---
+++ Mini Project Assignment_Y2S2/Views/Home/ChoosePostType.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Mini_Project_Assignment_Y2S2.Views.Home
+{
+    public class ChoosePoseTypeModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
